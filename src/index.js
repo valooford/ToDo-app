8b876eec324783c68feddbffbe1ecae3b5497f86
valooford <vladan.vip.vladan@gmail.com>
2020-04-21 20:18:21 +0300

    header and aside markup added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 import './styles/index.scss';
+import { setupIconedMenuItem, setupIconButton, setupNote } from './templates';
 
+// HEADER
+const header = document.querySelector('.header');
+const menuButton = ['&#xf0c9;', 'Главное меню', 'icon-button_bigger'];
+header.prepend(setupIconButton(...menuButton));
+
+const search = header.querySelector('.header__search *');
+const searchIconBlock = search.querySelector('.search__icon');
+const searchCleanBlock = search.querySelector('.search__clean');
+searchIconBlock.append(setupIconButton('&#xe814;', 'Поиск'));
+searchCleanBlock.append(
+  setupIconButton('&#xe80c;', 'Удалить поисковый запрос')
+);
+
+const headerButtonsBlock = header.querySelector('.header__buttons');
+const headerButtons = [
+  ['&#xe815;', 'Обновить', 'icon-button_bigger'],
+  ['&#xe819;', 'Сетка', 'icon-button_bigger'],
+  ['&#xe818;', 'Настройки', 'icon-button_bigger'],
+  ['&#xe816;', 'Приложения Google'],
+  ['V', 'Аккаунт Google'],
+];
+headerButtons.forEach((data) => {
+  headerButtonsBlock.append(setupIconButton(...data));
+});
+
+// ASIDE
+const aside = document.querySelector('.aside');
+const asideMenu = aside.querySelector('.aside__menu');
+const asideMenuButtons = [
+  ['&#xe80d;', 'Заметки', true],
+  ['&#xf0f3;', 'Напоминания'],
+  ['&#xe81d;', '123'],
+  ['&#xe80e;', 'Изменение ярлыков'],
+  ['&#xe805;', 'Архив'],
+  ['&#xe80f;', 'Корзина'],
+];
+asideMenuButtons.forEach((data) => {
+  asideMenu.append(setupIconedMenuItem(...data));
+});
+
+// MAIN
 const containerElement = document.getElementById('container');
 
 let focusedContainerItem;
@@ -46,28 +88,7 @@ function handleNoteFieldInput(e) {
 
 containerElement.addEventListener('input', handleNoteFieldInput, false);
 
-// TEMPLATES / ШАБЛОНЫ
-
-// ШАБЛОН КНОПКИ С ИКОНКОЙ / ICON-BUTTON
-const templateIconButton = document.getElementById('template-icon-button')
-  .firstElementChild;
-
-function setupIconButton(iconSymbol, titleText, modificator, disabled) {
-  const newIconButton = templateIconButton.cloneNode(true);
-  if (modificator) {
-    newIconButton.classList.add(modificator);
-  }
-  if (disabled) {
-    newIconButton.disabled = true;
-    newIconButton.tabIndex = -1;
-  }
-  const icon = newIconButton.firstElementChild;
-  icon.innerHTML = iconSymbol;
-  const title = newIconButton.querySelector('.title').firstElementChild;
-  title.textContent = titleText;
-
-  return newIconButton;
-}
+// ВСТАВКА ШАБЛОНОВ
 
 const addNoteButtonsBlock = document.querySelector(
   '.addNote .addNote__buttons'
@@ -80,108 +101,6 @@ const addNoteButtons = [
 addNoteButtons.forEach((data) => {
   addNoteButtonsBlock.append(setupIconButton(...data));
 });
-
-// ШАБЛОН ЭЛЕМЕНТА СПИСКА / LIST-ITEM
-
-const templateListItem = document.getElementById('template-list-item')
-  .firstElementChild;
-
-function setupListItem({ type } = {}) {
-  const newListItem = templateListItem.cloneNode(true);
-
-  const drag = newListItem.querySelector('.listItem__drag');
-  const add = newListItem.querySelector('.listItem__add');
-  const checkbox = newListItem.querySelector('.listItem__checkbox');
-  const text = newListItem.querySelector('.listItem__text');
-  const remove = newListItem.querySelector('.listItem__remove');
-  const closeButton = ['&#xe80c;', 'Удалить', 'icon-button_tiny'];
-
-  switch (type) {
-    case 'add':
-      drag.remove();
-      checkbox.remove();
-      remove.remove();
-      text.placeholder = 'Новый пункт';
-      break;
-    default:
-      add.remove();
-      remove.append(setupIconButton(...closeButton));
-      break;
-  }
-
-  return newListItem;
-}
-
-// ШАБЛОН ЗАМЕТКИ / NOTE
-
-const templateNote = document.getElementById('template-note').firstElementChild;
-
-function setupNote({ type } = {}) {
-  const newNote = templateNote.cloneNode(true);
-
-  const noteCheckBlock = newNote.querySelector('.note__check');
-  const noteCheck = [['&#xe80b;', 'Выбрать заметку', 'icon-button_no-padding']];
-  noteCheck.forEach((data) => {
-    noteCheckBlock.append(setupIconButton(...data));
-  });
-
-  const noteCornerButtonsBlock = newNote.querySelector('.note__cornerButtons');
-  const noteCornerButtons = [
-    ['&#xe812;', 'Закрепить заметку', 'icon-button_smaller'],
-  ];
-  noteCornerButtons.forEach((data) => {
-    noteCornerButtonsBlock.append(setupIconButton(...data));
-  });
-
-  const noteButtonsBlock = newNote.querySelector('.note__buttons');
-  const noteButtons = [
-    ['&#xf0f3;', 'Сохранить напоминание', 'icon-button_smaller'],
-    ['&#xe803;', 'Соавторы', 'icon-button_smaller'],
-    ['&#xe804;', 'Изменить цвет', 'icon-button_smaller'],
-    ['&#xe802;', 'Добавить картинку', 'icon-button_smaller'],
-    ['&#xe805;', 'Архивировать', 'icon-button_smaller'],
-    ['&#xe81f;', 'Ещё', 'icon-button_smaller'],
-    ['&#xe807;', 'Отменить', 'icon-button_smaller', true],
-    ['&#xe808;', 'Повторить', 'icon-button_smaller', true],
-  ];
-  noteButtons.forEach((data) => {
-    noteButtonsBlock.append(setupIconButton(...data));
-  });
-
-  const notificationClose = newNote.querySelector('.notification__close');
-  const closeButton = [
-    '&#xe80c;',
-    'Удалить напоминание',
-    'icon-button_notification',
-  ];
-  notificationClose.append(setupIconButton(...closeButton));
-
-  let list;
-  let markedList;
-
-  let listWrapper;
-
-  switch (type) {
-    case 'list':
-      newNote.querySelector('.textarea').remove();
-
-      list = newNote.querySelector('.note__list');
-      list.append(setupListItem());
-      list.append(setupListItem({ type: 'add' }));
-
-      markedList = newNote
-        .querySelector('.note__markedList')
-        .querySelector('.note__list');
-      markedList.append(setupListItem());
-      break;
-    default:
-      listWrapper = newNote.querySelector('.note__listWrapper');
-      listWrapper.remove();
-      break;
-  }
-
-  return newNote;
-}
 
 let lastContainerItem = document.querySelector('.container__item:last-of-type');
 
